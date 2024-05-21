ce7b3f2ed82176d7c8128b70cc8ccf56b4377c57
benhamza sofiane <benhamza4sofiane@gmail.com>
2024-05-21 23:48:54 +0100

    fixed

--- components/root/WhyUse.js
+++ components/root/WhyUse.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 const WhyUse = () => {
   return (
     <section className="section why-us" data-section="section2">
@@ -14,13 +15,13 @@ const WhyUse = () => {
                 <article id="tabs-1">
                   <div className="row">
                     <div className="col-md-6">
-                      <img src="/images/choose-us-image-01.png" alt="" />
+                      <Image src="/images/choose-us-image-01.png" alt="" />
                     </div>
                     <div className="col-md-6">
                       <h4>Best Education</h4>
                       <p>
                         At 2peerfect, the best education means delivering engaging and personalized learning experiences that cater to diverse interests and learning preferences.
-                        It's about empowering individuals with practical skills and theoretical knowledge relevant to IT, development, and languages, fostering curiosity and critical thinking along the way.
+                        It&apos;s about empowering individuals with practical skills and theoretical knowledge relevant to IT, development, and languages, fostering curiosity and critical thinking along the way.
                       </p>
                     </div>
                   </div>
@@ -28,7 +29,7 @@ const WhyUse = () => {
                 <article id="tabs-2">
                   <div className="row">
                     <div className="col-md-6">
-                      <img src="/images/choose-us-image-02.png" alt="" />
+                      <Image src="/images/choose-us-image-02.png" alt="" />
                     </div>
                     <div className="col-md-6">
                       <h4>Top Level</h4>
@@ -36,7 +37,7 @@ const WhyUse = () => {
                         Being at the top level for 2peerfect signifies offering cutting-edge resources, expert guidance, and a supportive community that enables learners to excel and become leaders in their chosen fields.
                       </p>
                       <p>
-                        It's about setting the standard for quality education and continuously innovating to meet the evolving needs of learners.
+                        It&apos;s about setting the standard for quality education and continuously innovating to meet the evolving needs of learners.
                       </p>
                     </div>
                   </div>
@@ -44,13 +45,13 @@ const WhyUse = () => {
                 <article id="tabs-3">
                   <div className="row">
                     <div className="col-md-6">
-                      <img src="/images/choose-us-image-03.png" alt="" />
+                      <Image src="/images/choose-us-image-03.png" alt="" />
                     </div>
                     <div className="col-md-6">
                       <h4>Quality Meeting</h4>
                       <p>
                         Quality meetings at 2peerfect are marked by effective communication, active collaboration, and clear objectives. They provide a platform for meaningful discussions, idea exchange,
-                        and decision-making, ensuring that every participant's input is valued and contributes to the continuous improvement of the educational experience.
+                        and decision-making, ensuring that every participant&apos;s input is valued and contributes to the continuous improvement of the educational experience.
                       </p>
                     </div>
                   </div>
